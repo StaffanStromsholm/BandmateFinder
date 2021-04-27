eb56eab46b4ba8c0b55f64af310eb43644bde35a
Staffan Strömsholm <52479640+StaffanStromsholm@users.noreply.github.com>
2021-04-27 15:05:10 +0300

    styled ViewUser component

--- src/components/ViewUser/ViewUser.js
+++ src/components/ViewUser/ViewUser.js
@@ -3,6 +3,7 @@ import { useParams } from 'react-router-dom';
 import { makeStyles, StylesProvider } from '@material-ui/core/styles';
 import styles from './ViewUser.module.scss';
 import * as api from '../../api/index.js';
+import { instruments } from '../../config';
 
 const useStyles = makeStyles({
     root: {
@@ -32,13 +33,22 @@ const ViewUser = () => {
         });
     }, []);
 
-    if (isLoading) {
+    if (isLoading || !user.lookingFor) {
         return <h1>Loading...</h1>
     }
 
     return (
             <div className={styles.ViewUser}>
-                {user.username}
+                <div className={styles.instrumentWrapper}><img className={styles.instrument} src={instruments[user.primaryInstrument]} /></div>
+                <div className={styles.infoWrapper}>
+                <h1>{user.username} </h1> 
+
+                <h3>{user.primaryInstrument}</h3>
+                <h3>{user.city}</h3>
+                <h3>Looking for: {user.lookingFor.bands && 'bands'}{user.lookingFor.jams && ', jams'}{user.lookingFor.studioWork && ', studio work'}{user.lookingFor.songWriting && ', song writing'}</h3>
+                <h3>{user.skillLevel}</h3>
+                <h3>{user.freeText}</h3>
+                </div>
             </div>
     );
 }
